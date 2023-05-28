6121ab658fcf117b7096855aee212660e766145d
archContaminatr <mahov45na@mail.ru>
2023-05-28 18:12:36 +0600

    Comment in Application.java

--- src/main/java/com/example/springsecuritytask/Application.java
+++ src/main/java/com/example/springsecuritytask/Application.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+//Напишите пожалуйста, что выходит при попытке залогиниться,
+//потому что у меня всё работает
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
